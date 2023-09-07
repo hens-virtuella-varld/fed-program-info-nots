be49d3221270624b40f427fef1645e0fc09f5f4b
Regan Y <hens.virtuella.varld@gmail.com>
2023-09-07 22:29:24 +0200

    feat: :sparkles: wip: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import hyperIslandImg from '../assets/hyperIsland.svg';
+import { ReactComponent as ArrowSvg } from '../assets/arrow.svg';
+import { ReactComponent as HyperIslandSvg } from '../assets/hyperIsland.svg';
+import headerInfo from '../data/headerInfo.js';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>
+    <div className='header'>
       <div>
-        <a href='https://www.hyperisland.com/' title='Hyper Island'>
-          <img src={hyperIslandImg} alt='Hyper Island' />
+        <a
+          href='https://www.hyperisland.com/'
+          title='Hyper Island'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <HyperIslandSvg />
         </a>
       </div>
-      <div></div>
+      <div className='headerInfoWrapper'>
+        {headerInfo.main.map((item, index) => (
+          <div className='categoryWrapper'>
+            <div key={`${item}-${index}`} className='category'>
+              {item.category}
+            </div>
+            <ArrowSvg className='dropDownArrow' />
+          </div>
+        ))}
+        <div className='categoryWrapper'>
+          <div className='category'>More</div>
+          <ArrowSvg />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/data/headerInfo.js
@@ -0,0 +1,193 @@
+const headerInfo = {
+  main: [
+    {
+      category: 'School',
+      topics: [
+        {
+          topic: 'Browse all programs & courses',
+          link: 'https://www.hyperisland.com/programs-and-courses',
+          description: 'Start your lifelong learning journey at Hyper Island',
+        },
+        {
+          topic: 'Full-time diplomas',
+          link: 'https://www.hyperisland.com/full-time-programs',
+          description: 'For international and Yrkeshögskolan (Yh) students',
+        },
+        {
+          topic: 'Masters degrees',
+          link: 'https://www.hyperisland.com/masters-degree',
+          description: 'Study full-time or part-time while working',
+        },
+        {
+          topic: 'Upskill courses',
+          link: 'https://www.hyperisland.com/programs-and-courses/upskill-programs',
+          description:
+            'Upskill your competences while working, searching jobs, or running your own business',
+        },
+      ],
+    },
+    {
+      category: 'Courses',
+      topics: [
+        {
+          topic: 'Courses & training',
+          link: 'https://www.hyperisland.com/courses',
+        },
+        {
+          topic: 'Online courses',
+          link: 'https://www.hyperisland.com/online-courses',
+        },
+        {
+          topic: 'Executive education',
+          link: 'https://www.hyperisland.com/study-while-working',
+        },
+        {
+          topic: 'UK Apprenticeships',
+          link: 'https://www.hyperisland.com/leadership-apprenticeship-programmes',
+        },
+      ],
+    },
+    {
+      category: 'Business',
+      topics: [
+        {
+          topic: 'Transform your business',
+          link: 'https://www.hyperisland.com/business-solutions',
+          description:
+            'Hands-on consultation designed to unlock the full potential of your people and organization',
+        },
+        {
+          topic: 'Tailored Solutions Europe (B2B)',
+          link: 'https://www.hyperisland.com/business-solutions/tailored-solutions-europe',
+          description:
+            'Developing leaders for any challenge through life long learning and an explorers mindset.',
+        },
+        {
+          topic: 'Impactful workshops for teams (EU)',
+          link: 'https://www.hyperisland.com/business-solutions/bt-workshop-eu',
+          description:
+            'Tools for building trust, collaboration and performance.',
+        },
+        {
+          topic: 'Team up with our students',
+          link: 'https://www.hyperisland.com/business-solutions/learning-partnership',
+          description:
+            'Let Hyper Island students help solve your business challenges',
+        },
+        {
+          topic: 'Contact us',
+          link: 'https://www.hyperisland.com/business-solutions/contact',
+        },
+      ],
+    },
+  ],
+  more: [
+    {
+      category: 'Locations',
+      locations: [
+        {
+          location: 'Brazil',
+          link: 'https://www.hyperisland.com/locations/brazil',
+        },
+        {
+          location: 'Singapore',
+          link: 'https://www.hyperisland.com/locations/singapore',
+        },
+        {
+          location: 'Sweden',
+          link: 'https://www.hyperisland.com/locations/sweden',
+        },
+        {
+          location: 'United Kingdom',
+          link: 'https://www.hyperisland.com/locations/united-kingdom',
+        },
+        {
+          location: 'North America',
+          link: 'https://www.hyperisland.com/locations/north-america',
+        },
+        {
+          location: 'Online',
+          link: 'https://www.hyperisland.com/online-courses',
+        },
+      ],
+    },
+    {
+      category: 'Resources',
+      topics: [
+        {
+          topic: 'INSIGHTS',
+          subTopics: [
+            { subTopic: 'Blog', link: 'https://www.hyperisland.com/blog' },
+            { subTopic: 'Events', link: 'https://www.hyperisland.com/events' },
+            {
+              subTopic: 'Newsletter',
+              link: 'https://www.hyperisland.com/newsletter',
+            },
+          ],
+        },
+        {
+          topic: 'SUCCESS STIRUES',
+          subTopics: [
+            {
+              subTopic: 'Business case studies',
+              link: 'https://www.hyperisland.com/business-solutions/case',
+            },
+            {
+              subTopic: 'Student stories',
+              link: 'https://www.hyperisland.com/student-and-alumni-stories',
+            },
+          ],
+        },
+        {
+          topic: 'TOOLS',
+          subTopics: [
+            {
+              subTopic: 'Toolbox',
+              link: 'https://toolbox.hyperisland.com/',
+              description: 'Resources designed to help you unleash potential',
+            },
+            {
+              subTopic: 'Remote Working Toolbox',
+              link: 'https://www.hyperisland.com/business-solutions/remote-working-toolbox',
+              description:
+                'Tips and tools for working, facilitating and leading teams remotely',
+            },
+            { subTopic: 'Job board', link: 'https://jobs.hyperisland.com/' },
+            {
+              subTopic: 'For alumni',
+              link: 'https://www.hyperisland.com/alumni',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      category: 'About',
+      topics: [
+        {
+          topic: 'History & Philosophy',
+          link: 'https://www.hyperisland.com/about',
+        },
+        {
+          topic: 'Community & Impact',
+          link: 'https://www.hyperisland.com/community',
+        },
+        {
+          topic: 'Our campus locations',
+          link: 'https://www.hyperisland.com/locations',
+        },
+        {
+          topic: 'People at Hyper Island',
+          link: 'https://career.hyperisland.com/people',
+        },
+        {
+          topic: 'Diversity, inclusion, and belonging',
+          link: 'https://www.hyperisland.com/hyper-island-diversity-inclusion-and-belonging',
+        },
+      ],
+    },
+    { category: 'Contact', link: 'https://www.hyperisland.com/contact' },
+  ],
+};
+
+export default headerInfo;
